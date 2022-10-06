d95facb972d085013e8b81c303e49f7e4ddae184
isaacrmoreno <ipdxcreative@gmail.com>
2022-10-06 13:31:17 -0700

    Initial Commit

---
+++ Shared/tierraApp.swift
@@ -0,0 +1,17 @@
+//
+//  tierraApp.swift
+//  Shared
+//
+//  Created by Isaac Moreno on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct tierraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

cba20d5292079739b6adf711d03d4d9fd27badf5
isaacrmoreno <ipdxcreative@gmail.com>
2022-10-10 17:24:05 -0700

    Add library to test out other bottom sheet option. Might just add floating view with toggle

--- Shared/BottomSheetView.swift
+++ Shared/BottomSheetView.swift
@@ -5,21 +5,49 @@
 //  Created by Isaac Moreno on 10/10/22.
 //
 
+//import SwiftUI
+//
+//struct BottomSheetView: View {
+//    @State var presentSheet = false
+//
+//    var body: some View {
+//        NavigationView {
+//            Button("Modal") {
+//                presentSheet = true
+//            }
+//            .navigationTitle("Main")
+//        }.sheet(isPresented: $presentSheet) {
+//            Text("Detail")
+////                .presentationDetents([.medium])
+//        }
+//    }
+//}
+
 import SwiftUI
+import BottomSheet
 
 struct BottomSheetView: View {
-    @State var presentSheet = false
-
+    @State var bottomSheetPosition: BottomSheetPosition = .absolute(325)
+    
+    let backgroundColors: [Color] = [Color(red: 0.2, green: 0.85, blue: 0.7), Color(red: 0.13, green: 0.55, blue: 0.45)]
+    let readMoreColors: [Color] = [Color(red: 0.70, green: 0.22, blue: 0.22), Color(red: 1, green: 0.32, blue: 0.32)]
+    let bookmarkColors: [Color] = [Color(red: 0.28, green: 0.28, blue: 0.53), Color(red: 0.44, green: 0.44, blue: 0.83)]
+    
     var body: some View {
-        NavigationView {
-            Button("Modal") {
-                presentSheet = true
-            }
-            .navigationTitle("Main")
-        }.sheet(isPresented: $presentSheet) {
-            Text("Detail")
-//                .presentationDetents([.medium])
-        }
+        //A green gradient as a background that ignores the safe area.
+        LinearGradient(gradient: Gradient(colors: self.backgroundColors), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(.all)
+        
+            .bottomSheet(bottomSheetPosition: self.$bottomSheetPosition, switchablePositions: [
+                .dynamicBottom,
+                .absolute(325)
+            ],
+            headerContent: {}) {}
+            .showDragIndicator(false)
+            .enableContentDrag()
+//            .showCloseButton()
+//            .enableSwipeToDismiss()
+//            .enableTapToDismiss()
     }
 }
 
--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         ZStack {
             MapBoxMapView()
             StackIconView()
-//            BottomSheetView()
+            BottomSheetView()
         }
     }
 }
